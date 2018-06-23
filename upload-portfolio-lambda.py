a581b9860e769a88b6d35eeda30b3df0305344ec
Joe Hebl <joe.hebl1@gmail.com>
2018-06-23 09:51:52 -0500

    Fixed lambda code to work with CodeDeploy properly

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,4 +1,5 @@
 import boto3
+from botocore.client import Config
 import StringIO
 import zipfile
 import mimetypes
@@ -7,14 +8,28 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:846480965704:deployPortfolioTopic')
 
+    location = {
+        "bucketName": 'portfoliobuild.stage0.io',
+        "objectKey": 'portfoliobuild.zip'
+    }
+
     try:
-        s3 = boto3.resource('s3')
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+
+        print "Building portfolio from: " + str(location)
+
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
         portfolio_bucket = s3.Bucket('portfolio.stage0.io')
-        build_bucket = s3.Bucket('portfoliobuild.stage0.io')
+        build_bucket = s3.Bucket(location["bucketName"])
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -24,8 +39,12 @@ def lambda_handler(event, context):
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
 
-        print 'Job Complete'
+        print "Job Done!"
         topic.publish(Subject="Portfolio Deployed", Message = "Portfolio deployed successfully.")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+
     except:
         topic.publish(Subject="Portfolio Deploy Failed", Message = "The portfolio was not deployed successfully.")
         raise
